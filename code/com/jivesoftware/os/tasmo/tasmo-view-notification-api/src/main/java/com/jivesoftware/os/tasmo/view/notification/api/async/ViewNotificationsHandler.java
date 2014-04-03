0e4daa81e128a278842f0446457a7bb3fe090763
Jonathan Colt <jonathan.colt@jivesoftware.com>
2014-04-03 19:13:57 -0400

    added in async view notofication api

---
+++ code/com/jivesoftware/os/tasmo/tasmo-view-notification-api/src/main/java/com/jivesoftware/os/tasmo/view/notification/api/async/ViewNotificationsHandler.java
@@ -0,0 +1,16 @@
+package com.jivesoftware.os.tasmo.view.notification.api.async;
+
+import com.jivesoftware.os.tasmo.id.BaseView;
+import com.jivesoftware.os.tasmo.view.notification.api.ViewNotification;
+import com.jivesoftware.os.tasmo.view.notification.api.ViewNotification;
+import com.jivesoftware.os.tasmo.view.notification.api.async.ViewNotificationsCallback;
+import java.util.List;
+
+/**
+ * Takes a batch of view change notifications and invokes a callback for each view.
+ */
+public interface ViewNotificationsHandler<V extends BaseView<?>> {
+
+    void handleNotifications(List<ViewNotification> viewNotifications, ViewNotificationsCallback<V> callback);
+
+}
