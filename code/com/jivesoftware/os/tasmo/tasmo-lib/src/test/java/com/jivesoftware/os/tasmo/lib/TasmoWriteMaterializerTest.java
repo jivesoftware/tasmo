4df2ba71e13b224d92ee6898671b384a6238ffd4
Jonathan Colt <jonathan.colt@jivesoftware.com>
2014-07-10 06:30:47 -1000

    fixed test that wasn't compiling

--- code/com/jivesoftware/os/tasmo/tasmo-lib/src/test/java/com/jivesoftware/os/tasmo/lib/TasmoWriteMaterializerTest.java
+++ code/com/jivesoftware/os/tasmo/tasmo-lib/src/test/java/com/jivesoftware/os/tasmo/lib/TasmoWriteMaterializerTest.java
@@ -3,7 +3,9 @@ package com.jivesoftware.os.tasmo.lib;
 import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.common.util.concurrent.MoreExecutors;
 import com.jivesoftware.os.jive.utils.base.interfaces.CallbackStream;
+import com.jivesoftware.os.tasmo.lib.process.TasmoEventProcessor;
 import com.jivesoftware.os.tasmo.lib.process.bookkeeping.BookkeepingEvent;
+import com.jivesoftware.os.tasmo.lib.write.TasmoWriteMaterializer;
 import com.jivesoftware.os.tasmo.model.process.WrittenEvent;
 import java.util.Collections;
 import java.util.List;
@@ -12,20 +14,20 @@ import org.mockito.Mockito;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class TasmoViewMaterializerTest {
+public class TasmoWriteMaterializerTest {
 
     private static final ListeningExecutorService LES = MoreExecutors.listeningDecorator(Executors.newSingleThreadExecutor());
     private CallbackStream<List<BookkeepingEvent>> callbackStream;
     private TasmoEventProcessor tasmoEventProcessor;
     private TasmoBlacklist tasmoBlacklist;
-    private TasmoViewMaterializer tasmoViewMaterializer;
+    private TasmoWriteMaterializer tasmoViewMaterializer;
 
     @BeforeMethod
     public void beforeMethod() throws Exception {
         callbackStream = Mockito.mock(CallbackStream.class);
         tasmoEventProcessor = Mockito.mock(TasmoEventProcessor.class);
         tasmoBlacklist = new TasmoBlacklist();
-        tasmoViewMaterializer = new TasmoViewMaterializer(callbackStream, tasmoEventProcessor, LES, tasmoBlacklist);
+        tasmoViewMaterializer = new TasmoWriteMaterializer(callbackStream, tasmoEventProcessor, LES, tasmoBlacklist);
     }
 
     @Test
